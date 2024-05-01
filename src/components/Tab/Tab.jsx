bd9baf731b9e8aa710eddf50b3d46de13ea48bca
Lin <shl6534@naver.com>
2024-05-01 15:26:53 +0900

    Refactor: Tab 컴포넌트 리뷰 반영

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import styles from "./Tab.module.scss";
 
 export default function Tab() {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const menuArr = ["이달의 여자 아이돌", "이달의 남자 아이돌"];
+
   return (
-    <div className={styles.tab}>
-      <span>이달의 여자 아이돌</span>
+    <div>
+      <ul className={styles.tabContainer}>
+        {menuArr.map((menu, index) => (
+          <li
+            key={index}
+            className={`${styles.tab} ${index === currentTab ? styles.tabFocused : ""}`}
+            onClick={() => setCurrentTab(index)}
+          >
+            {menuArr[index]}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
