033b8221e52fd894699ba55adfbc69567d172560
Lin <shl6534@naver.com>
2024-05-01 15:44:43 +0900

    Feat: className 라이브러리 사용

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import cn from 'classnames';
 import styles from "./Tab.module.scss";
 
 export default function Tab() {
@@ -12,7 +13,7 @@ export default function Tab() {
         {menuArr.map((menu, index) => (
           <li
             key={index}
-            className={`${styles.tab} ${index === currentTab ? styles.tabFocused : ""}`}
+            className={cn(styles.tab, { [styles.tabFocused]: index === currentTab })}
             onClick={() => setCurrentTab(index)}
           >
             {menuArr[index]}
