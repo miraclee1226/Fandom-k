5cd901ef86068980ec551507e36cdb1d58857c13
Lee Youl <2yulrang@gmail.com>
2024-05-01 14:15:54 +0900

    feat: supportSection 추가

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,3 +1,66 @@
+import { useRef } from "react";
+import SupportCard from "../components/Card/SupportCard";
+import styles from "./List.module.scss";
+
 export default function List() {
-  return <></>;
+  const slider = useRef();
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  const handleMouseDown = (e) => {
+    isDown = true;
+    startX = e.pageX - slider.current.offsetLeft;
+    scrollLeft = slider.current.scrollLeft;
+  };
+
+  const handleMouseLeave = (e) => {
+    isDown = false;
+  };
+
+  const handleMouseUp = (e) => {
+    isDown = false;
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.current.offsetLeft;
+    const walk = x - startX;
+
+    slider.current.scrollLeft = scrollLeft - walk;
+  };
+
+  return (
+    <>
+      <div className={styles.view}>
+        <section className={styles.section}>
+          <h2 className={styles.sectionTitle}>후원을 기다리는 조공</h2>
+          <div className={styles.sectionContent}>
+            <ul
+              className={styles.supportLists}
+              ref={slider}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+              onMouseMove={handleMouseMove}
+              onMouseLeave={handleMouseLeave}
+            >
+              <li className={styles.supportList}>
+                <SupportCard />
+              </li>
+              <li className={styles.supportList}>
+                <SupportCard />
+              </li>
+              <li className={styles.supportList}>
+                <SupportCard />
+              </li>
+              <li className={styles.supportList}>
+                <SupportCard />
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
