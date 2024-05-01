0f2fb7dc9592fbf0db530ffb45a37e9d8e5809e1
alexgoni <goni000211@gmail.com>
2024-05-01 17:02:20 +0900

    Feat: Credit Section Skeleton

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,3 +1,29 @@
+import Navbar from "components/Navbar/Navbar";
+import creditIcon from "assets/icons/credit.svg";
+import styles from "./List.module.scss";
+
 export default function List() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <CreditSection />
+    </>
+  );
+}
+
+// TODO: Util 함수
+
+function CreditSection() {
+  return (
+    <div className={styles.creditSection}>
+      <div className={styles.leftContent}>
+        <span>내 크레딧</span>
+        <div className={styles.credit}>
+          <img src={creditIcon} alt="credit" />
+          <span>36,000</span>
+        </div>
+      </div>
+      <div className={styles.rightContent}>충전하기</div>
+    </div>
+  );
 }
