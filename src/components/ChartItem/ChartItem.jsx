c4a629d3c1da7f91dab86805b143a4ddb1c26e1d
Lin <shl6534@naver.com>
2024-05-01 20:35:37 +0900

    Refactor: 리뷰 내용 반영

--- src/components/ChartItem/ChartItem.jsx
+++ src/components/ChartItem/ChartItem.jsx
@@ -7,7 +7,7 @@ export default function ChartItem() {
       <div className={styles.content}>
         <RoundImage />
         <span className={styles.ranking}>1</span>
-        <span className={styles.name}>르세라핌 채원</span>
+        <strong className={styles.name}>르세라핌 채원</strong>
       </div>
       <span className={styles.votes}>204,000표</span>
     </div>
