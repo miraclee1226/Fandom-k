b3a10cb21a2768a3bd191917114c37d5dc41d0e3
Lin <shl6534@naver.com>
2024-05-01 11:29:45 +0900

    Feat: RoundImage 컴포넌트 개발

---
+++ src/components/Image/RoundImage/RoundImage.jsx
@@ -0,0 +1,10 @@
+import styles from "./RoundImage.module.scss";
+import IdolImg from "../../../assets/Image_Idol.png";
+
+export default function RoundImage() {
+  return (
+    <div className={styles.roundImage}>
+      <img src={IdolImg} alt="르세라핌 채원 이미지" />
+    </div>
+  );
+}
