6a6a787207ca8b34fac2b41e9cfe25b68c4aed6b
Lee Youl <2yulrang@gmail.com>
2024-05-02 12:24:25 +0900

    Design: MyPage 공통부분 구현, ListPage 공통CSS 미디어쿼리 수정

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,10 +2,5 @@ import Navbar from "components/Navbar/Navbar";
 import SupportCard from "components/Card/SupportCard";
 
 export default function Landing() {
-  return (
-    <>
-      <Navbar />
-      <SupportCard />;
-    </>
-  );
+  return <></>;
 }
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,16 +1,37 @@
-import Button from "../components/Button";
+import styles from "pages/MyPage.module.scss";
+import Navbar from "components/Navbar/Navbar";
+import Layout from "components/Layout/Layout";
 
 export default function MyPage() {
   return (
     <>
-      <Button>후원하기</Button>
+      <Navbar />
+      <Layout>
+        <AddFavoriteSection />
+        <AddFavoriteSection />
+      </Layout>
+
+      {/* <Button>후원하기</Button>
       <Button disabled>후원하기</Button>
       <Button size="sm">차트 투표하기</Button>
       <Button.Border>더 보기</Button.Border>
       <Button.Arrow direction="left" />
       <Button.Arrow direction="right" size="lg" />
       <Button.Round>+ 추가하기</Button.Round>
-      <Button.Radio />
+      <Button.Radio /> */}
+    </>
+  );
+}
+
+function AddFavoriteSection() {
+  return (
+    <>
+      <section className={styles.section}>
+        <h2 className={styles.sectionTitle}>
+          관심 있는 아이돌을 추가해보세요.
+        </h2>
+        <div className={styles.sectionContent}></div>
+      </section>
     </>
   );
 }
