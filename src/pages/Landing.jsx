f8f1fdedf05e601aea5b2bcc3100ba8ff2b7c8ca
Lin <shl6534@naver.com>
2024-05-02 10:42:42 +0900

    Chore: Landing 컴포넌트 변경

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,11 @@
-import Navbar from "../components/Navbar/Navbar";
-import SupportCard from "../components/Card/SupportCard";
+import Navbar from "components/Navbar/Navbar";
+import List from "pages/List";
 
 export default function Landing() {
   return (
     <>
       <Navbar />
-      <SupportCard />;
+      <List />
     </>
   );
 }
