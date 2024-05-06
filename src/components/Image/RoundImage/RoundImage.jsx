7bdd0b0e9b6f2f47feebac19dee9ee17d3e91d1a
Lin <shl6534@naver.com>
2024-05-06 19:28:55 +0900

    Feat: RoundImage, RectangleImage 컴포넌트 이미지 최적화

--- src/components/Image/RoundImage/RoundImage.jsx
+++ src/components/Image/RoundImage/RoundImage.jsx
@@ -1,10 +1,21 @@
+import { useState, useEffect } from "react";
 import styles from "components/Image/Image.module.scss";
-import IdolImg from "assets/Image_Idol.png";
+import WebpLoader from "components/WebpLoader";
 
-export default function RoundImage() {
+export default function RoundImage({ src, lazyMode = false }) {
+  let webpSrc = null;
+
+  if (src.endsWith(".webp")) {
+    webpSrc = src;
+  }
+
   return (
     <div className={styles.roundImage}>
-      <img src={IdolImg} alt="르세라핌 채원 이미지" />
+      {lazyMode ? (
+        <WebpLoader.Lazy src={src} webpSrc={webpSrc} />
+      ) : (
+        <WebpLoader src={src} webpSrc={webpSrc} />
+      )}
     </div>
   );
 }
