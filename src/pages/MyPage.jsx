08fdb6b7cdeaee09631d9c02c9f52c4ab431ff28
Lee Youl <2yulrang@gmail.com>
2024-05-06 14:08:18 +0900

    Design: addFavoriteSection 구조 수정, 아래 여백 설저ㅓㅇ

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -32,44 +32,46 @@ function AddFavoriteSection({ idols }) {
         </h2>
         <div className={styles.sectionContent}>
           <div className={styles.AddFavorite}>
-            <div className={styles.sliderBtn}>
-              <Button.Arrow direction="left" size="lg" />
+            <div className={styles.sliderWrap}>
+              <div className={styles.sliderBtn}>
+                <Button.Arrow direction="left" size="lg" />
+              </div>
+              <div className={styles.slider}>
+                <ul className={styles.gridContainer}>
+                  {idols.map((idol) => {
+                    return (
+                      <li key={idol.id} className={styles.gridItem}>
+                        <input
+                          type="checkbox"
+                          id={idol.id}
+                          className={styles.chkItem}
+                        />
+                        <label htmlFor={idol.id} className={styles.labelItem}>
+                          <div className={styles.imgWrap}>
+                            <RoundImage />
+                          </div>
+                        </label>
+                        <span className={styles.itemInfo}>
+                          <strong className={styles.itemTitle}>
+                            {idol.name}
+                          </strong>
+                          <p className={styles.itemCategory}>{idol.group}</p>
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div className={styles.sliderBtn}>
+                <Button.Arrow direction="right" size="lg" />
+              </div>
             </div>
-            <div className={styles.slider}>
-              <ul className={styles.gridContainer}>
-                {idols.map((idol) => {
-                  return (
-                    <li key={idol.id} className={styles.gridItem}>
-                      <input
-                        type="checkbox"
-                        id={idol.id}
-                        className={styles.chkItem}
-                      />
-                      <label htmlFor={idol.id} className={styles.labelItem}>
-                        <div className={styles.imgWrap}>
-                          <RoundImage />
-                        </div>
-                      </label>
-                      <span className={styles.itemInfo}>
-                        <strong className={styles.itemTitle}>
-                          {idol.name}
-                        </strong>
-                        <p className={styles.itemCategory}>{idol.group}</p>
-                      </span>
-                    </li>
-                  );
-                })}
-              </ul>
+            <div className={styles.btnAddFavorite}>
+              <Button.Round>
+                <img src={icoPlus} alt="icon" aria-hidden="true" />
+                <span>추가하기</span>
+              </Button.Round>
             </div>
-            <div className={styles.sliderBtn}>
-              <Button.Arrow direction="right" size="lg" />
-            </div>
-          </div>
-          <div className={styles.btnAddFavorite}>
-            <Button.Round>
-              <img src={icoPlus} alt="icon" aria-hidden="true" />
-              <span>추가하기</span>
-            </Button.Round>
           </div>
         </div>
       </section>
