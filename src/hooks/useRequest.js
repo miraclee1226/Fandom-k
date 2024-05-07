1569183ab4abda1354d03e02f22739d0b24c61b3
alexgoni <goni000211@gmail.com>
2024-05-07 14:51:24 +0900

    Docs: useRequest hook 주석

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -2,11 +2,12 @@ import dispatcher from "api/dispatcher";
 import { useEffect, useState } from "react";
 
 /**
- * 비동기 함수를 감싼 함수를 반환하는 Hook입니다.
- * pending과 error 상태를 관리합니다.
+ * Data Fetching Hook
  *
- * @param {Function} asyncFunc - 비동기 함수
- * @returns {Object} - { wrappedFunc, pending, error }
+ * @param {options} options - axios instance에 전달할 options
+ * @param {skip} skip - true: 마운트될 때 data fetching 금지
+ * @param {deps} deps - useEffect 의존성 배열
+ * @returns {Object} Object - { data, isLoading, error, requestFunc  }
  */
 export default function useRequest({ options, skip = false, deps = [] }) {
   const [data, setData] = useState(null);
