3d7e594d422ecf97229443da4152086887ab18a8
alexgoni <goni000211@gmail.com>
2024-05-07 14:36:51 +0900

    Refactor: useRequest hook 수정

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,26 +1,38 @@
+import dispatcher from "api/dispatcher";
 import { useEffect, useState } from "react";
 
-export default function useRequest(requestFunc, deps = []) {
-  const [response, setResponse] = useState(null);
+/**
+ * 비동기 함수를 감싼 함수를 반환하는 Hook입니다.
+ * pending과 error 상태를 관리합니다.
+ *
+ * @param {Function} asyncFunc - 비동기 함수
+ * @returns {Object} - { wrappedFunc, pending, error }
+ */
+export default function useRequest({ options, skip = false, deps = [] }) {
+  const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const requestFunc = async (...args) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await dispatcher({ ...options, ...args });
+
+      setData(response);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      setError(null);
-
-      try {
-        const res = await requestFunc();
-
-        setResponse(res);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
+    if (skip) return;
+    requestFunc();
   }, deps);
 
-  return { response, isLoading, error };
+  return { data, isLoading, error, requestFunc };
 }
+
