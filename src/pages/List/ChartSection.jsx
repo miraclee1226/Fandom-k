38e97315b2964277c312e9c08b5f515448b83edb
Lin <shl6534@naver.com>
2024-05-08 11:21:04 +0900

    Feat: ChartSection api 연동

--- src/pages/List/ChartSection.jsx
+++ src/pages/List/ChartSection.jsx
@@ -1,43 +1,76 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Tab from "components/Tab";
 import ChartItem from "components/ChartItem";
 import Button from "components/Button";
 import { ReactComponent as ChartIcon } from "assets/icons/chart.svg";
 import femaleData from "mockData/femaleData";
 import maleData from "mockData/maleData";
+import Modal from "components/Modal";
+import useRequest from "hooks/useRequest";
 import styles from "./List.module.scss";
 
 export default function ChartSection() {
-  const [sortedData, setSortedData] = useState(
-    [...femaleData].sort((a, b) => b.totalVotes - a.totalVotes),
-  );
-
+  const [gender, setGender] = useState("female");
+  const [pageSize, setPageSize] = useState(20);
+  const [sortedData, setSortedData] = useState([]);
+  const { data, requestFunc: getChartData } = useRequest({
+    skip: true,
+    options: {
+      method: "get",
+      url: `/charts/${gender}`,
+      params: {
+        gender,
+        pageSize,
+      },
+    },
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  
   const handleTabChange = (index) => {
-    const selectedGender = index === 0 ? [...femaleData] : [...maleData];
-
-    setSortedData(selectedGender.sort((a, b) => b.totalVotes - a.totalVotes));
+    setGender(index === 0 ? "female" : "male");
   };
 
+  const handleButtonClick = () => setPageSize((prev) => prev + 10);
+
+  const handleModalOpen = () => {
+    setIsOpen(!isOpen);
+    getChartData();
+  };
+
+  useEffect(() => {
+    (async () => {
+      const result = await getChartData();
+
+      setSortedData(result?.data.idols);
+    })();
+  }, [gender, pageSize, isOpen]);
+  
   return (
     <section className={styles.section}>
       <div className={styles.sectionContainer}>
         <div className={styles.sectionHeader}>
           <h2 className={styles.sectionTitle}>이달의 차트</h2>
-          <Button.Round className={styles.voteButton}>
+          <Button.Round className={styles.voteButton} onClick={handleModalOpen}>
             <ChartIcon />
             차트 투표하기
           </Button.Round>
+          <Modal.Vote data={data?.data?.idols} isOpen={isOpen} handleModalOpen={handleModalOpen} gender={gender} />
         </div>
         <div className={styles.sectionDetail}>
           <div className={styles.tab}>
             <Tab handleTabChange={handleTabChange} />
           </div>
           <ul className={styles.chartList}>
-            {sortedData.map((data, index) => (
-              <ChartItem key={data.id} data={data} />
+            {sortedData.map((chartItem, index) => (
+              <ChartItem key={chartItem.id} data={chartItem} />
             ))}
           </ul>
-          <Button.Border className={styles.borderButton}>더보기</Button.Border>
+          <Button.Border
+            onClick={handleButtonClick}
+            className={styles.borderButton}
+          >
+            더보기
+          </Button.Border>
         </div>
       </div>
     </section>
