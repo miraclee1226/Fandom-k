e573b29e6b876910747ce790a79088cd8ed8ca85
eundonghyeok <mephisto7191@naver.com>
2024-05-10 16:39:52 +0900

    Fix: logo 클릭 시 새로고침

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,17 @@ import profile from "assets/profile.webp";
 import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
+  const reload = () => window.location.reload();
+
   return (
     <nav className={styles.navbar}>
       <div></div>
-      <Link to="/list">
-        <img className={styles.navbarLogo} src={logo} alt="로고" />
-      </Link>
+      <img
+        onClick={reload}
+        className={styles.navbarLogo}
+        src={logo}
+        alt="로고"
+      />
 
       <Link to="/mypage">
         <img className={styles.navbarProfile} src={profile} alt="프로필" />
@@ -19,3 +24,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
