3cf0d4de4ba128ca0ffa58f3579fd16e91349e29
Lee Youl <2yulrang@gmail.com>
2024-05-14 12:20:04 +0900

    Fix: 레이지로딩 디폴트 이미지 추가

--- src/components/Image/RectangleImage/RectangleImage.jsx
+++ src/components/Image/RectangleImage/RectangleImage.jsx
@@ -4,19 +4,18 @@ import styles from "../Image.module.scss";
 
 export default function RectangleImage({ src, alt, lazyMode = false }) {
   let webpSrc = null;
-  
+
   if (src.endsWith(".webp")) {
     webpSrc = src;
   }
-  
+
   return (
     <div className={styles.rectangleImage}>
-    {lazyMode ? (
-      <WebpLoader.Lazy src={src} webpSrc={src} alt={alt} />
-    ) : (
-      <WebpLoader src={src} webpSrc={src} alt={alt} />
-    )}
+      {lazyMode ? (
+        <WebpLoader.Lazy src={src} webpSrc={src} alt={alt} />
+      ) : (
+        <WebpLoader src={src} webpSrc={src} alt={alt} />
+      )}
     </div>
   );
 }
-
